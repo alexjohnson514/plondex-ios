69fdf889305f56078d385150723ff6b454898329
Alex Johnson <alexjohnson@Alexs-Mac.local>
2016-06-08 10:08:32 -0700

    Initial Commit

---
+++ HappyChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HappyChat
+//
+//  Created by Alex Johnson on 8/06/2016.
+//  Copyright (c) 2016 NikolaiTomov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HappyChat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HappyChat
+//
+//  Created by Alex Johnson on 8/06/2016.
+//  Copyright (c) 2016 NikolaiTomov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
